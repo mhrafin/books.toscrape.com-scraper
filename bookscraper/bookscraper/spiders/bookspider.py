31025b17153a902a255667fab50aeb34be21d271
rafwork <110781737+mhrafin@users.noreply.github.com>
2025-10-09 17:22:46 +0600

    Spider can now srcape all books

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,18 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+        for book in books:
+            yield{
+                'name': book.css('h3 a::text').get(),
+                'price': book.css('div.product_price p.price_color::text').get(),
+                'url': book.css('h3 a').attrib['href'],
+            }
+
+        next_page = response.css('li.next a').attrib['href']
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse) 
